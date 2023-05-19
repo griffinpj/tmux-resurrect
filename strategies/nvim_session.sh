77b131ccfb09a1f8bbc7eae25526d4d4e80ab17a
Griffin Johnson <72412460+griffinpj@users.noreply.github.com>
2023-05-19 15:30:11 -0700

    Update nvim_session.sh

--- strategies/nvim_session.sh
+++ strategies/nvim_session.sh
@@ -17,7 +17,7 @@ original_command_contains_session_flag() {
 
 main() {
 	if nvim_session_file_exists; then
-		echo "nvim -S"
+		echo "nvim -S .session.vim"
 	elif original_command_contains_session_flag; then
 		# Session file does not exist, yet the original nvim command contains
 		# session flag `-S`. This will cause an error, so we're falling back to
