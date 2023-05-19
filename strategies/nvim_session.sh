443e46dbf5c8207047f782d6fe3f864682b58834
Griffin Johnson <72412460+griffinpj@users.noreply.github.com>
2023-05-19 15:06:13 -0700

    Update nvim_session.sh

--- strategies/nvim_session.sh
+++ strategies/nvim_session.sh
@@ -8,7 +8,7 @@ ORIGINAL_COMMAND="$1"
 DIRECTORY="$2"
 
 nvim_session_file_exists() {
-	[ -e "${DIRECTORY}/Session.vim" ]
+	[ -e "${DIRECTORY}/.session.vim" ]
 }
 
 original_command_contains_session_flag() {
